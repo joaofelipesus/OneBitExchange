6492cc2798787ed646315a98f750c2721203815e
joao_felipe <joaofelipe1294@gmail.com>
2019-12-26 10:05:54 -0300

    Corrigida importação do bootstrap-js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
+//= require bootstrap/dist/js/bootstrap.js
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'exchanges/index'
-  get 'exchanges/convert'
+  # root 'exchanges/index', to: 'exchanges#index'
+  # get 'exchanges/convert', to: 'exchanges#convert'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'exchanges#index'
+  get 'convert', to: 'exchanges#convert'
 end
