357074caf78d7441a9126a373814d188b822ed92
joao_felipe <joaofelipe1294@gmail.com>
2019-12-26 17:24:30 -0300

    Adicionado inicio da chamada a API que busca os valores de bitcoin.

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -10,16 +10,44 @@ class ExchangeService
 
 
   def perform
-    begin
-      exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
-      exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
-      url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
-      res = RestClient.get url
-      value = JSON.parse(res.body)['currency'][0]['value'].to_f
-
-      value * @amount
-    rescue RestClient::ExceptionWithResponse => e
-      e.response
+    if @source_currency != 'BTC' && @target_currency != 'BTC'
+      exchange_api_key()
+    else
+      calc_currency_to_bitcoin() if @target_currency == 'BTC'
+      if @source_currency == 'BTC'
+        url = "https://blockchain.info/tobtc?currency=#{@target_currency}&value=#{1}"
+        puts url
+        res = RestClient.get url
+        reference_value = JSON.parse(res.body).to_f
+        return (reference_value * @amount)
+      end
     end
   end
+
+  private
+
+    def exchange_api_call
+      begin
+        exchange_api_url = Rails.application.credentials[Rails.env.to_sym][:currency_api_url]
+        exchange_api_key = Rails.application.credentials[Rails.env.to_sym][:currency_api_key]
+        url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
+        res = RestClient.get url
+        value = JSON.parse(res.body)['currency'][0]['value'].to_f
+        value * @amount
+      rescue RestClient::ExceptionWithResponse => e
+        e.response
+      end
+    end
+
+    def calc_currency_to_bitcoin
+      url = "https://blockchain.info/tobtc?currency=#{@source_currency}&value=#{@amount}"
+      res = RestClient.get url
+      begin
+        return JSON.parse(res.body).to_f
+      rescue RestClient::ExceptionWithResponse => e
+        e.response
+      end
+    end
+
+
 end
